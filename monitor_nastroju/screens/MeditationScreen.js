cf90637251f81bb5b3f34c0800e19e532b7fbca4
Natalia-2111 <nataliawegrzynska@o2.pl>
2025-12-09 18:42:34 +0100

    update MeditationScreen

--- monitor_nastroju/screens/MeditationScreen.js
+++ monitor_nastroju/screens/MeditationScreen.js
@@ -1,40 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import colors from '../styles/colors';
-
-
+import { ThemeContext } from '../ThemeContext';
 
 export default function MeditationScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate('MeditationSession')}
-      >
-        <Text style={styles.buttonText}>Rozpocznij medytację</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    const { theme } = useContext(ThemeContext);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: colors.background,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  button: {
-    backgroundColor: colors.accent,
-    paddingVertical: 15,
-    paddingHorizontal: 40,
-    borderRadius: 30,
-    elevation: 4,
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 18,
-    fontWeight: '600',
-    textTransform: 'uppercase',
-  },
-});
+    const styles = StyleSheet.create({
+        container: { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: theme.background },
+        button: {
+            backgroundColor: theme.accent,
+            paddingVertical: 15,
+            paddingHorizontal: 40,
+            borderRadius: 30,
+            elevation: 4,
+        },
+        buttonText: { color: '#fff', fontSize: 18, fontWeight: '600', textTransform: 'uppercase' },
+    });
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('MeditationSession')}>
+                <Text style={styles.buttonText}>Rozpocznij medytację</Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
