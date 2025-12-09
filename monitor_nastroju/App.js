d8ab6d0dfcd0f85ca270b1e59d2bc0a63155f8f5
Natalia-2111 <nataliawegrzynska@o2.pl>
2025-12-09 18:54:25 +0100

    import rejestracji

--- monitor_nastroju/App.js
+++ monitor_nastroju/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { ThemeProvider } from './ThemeContext';
 import LoginScreen from './screens/LoginScreen';
 import HomeScreen from './screens/HomeScreen';
 import EntryScreen from './screens/EntryScreen';
@@ -10,23 +11,27 @@ import GalleryScreen from './screens/GalleryScreen';
 import QuoteScreen from './screens/QuoteScreen';
 import OptionsScreen from './screens/OptionsScreen';
 import MeditationSession from './screens/MeditationSession';
+import RegisterScreen from './screens/RegisterScreen'; 
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Entry" component={EntryScreen} />
-        <Stack.Screen name="Statistics" component={StatisticsScreen} />
-        <Stack.Screen name="Meditation" component={MeditationScreen} />
-        <Stack.Screen name="Gallery" component={GalleryScreen} />
-        <Stack.Screen name="Quote" component={QuoteScreen} />
-        <Stack.Screen name="Options" component={OptionsScreen} />
-        <Stack.Screen name="MeditationSession" component={MeditationSession} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <ThemeProvider>
+          <NavigationContainer>
+              <Stack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+                  <Stack.Screen name="Login" component={LoginScreen} />
+                  <Stack.Screen name="Register" component={RegisterScreen} />
+                  <Stack.Screen name="Home" component={HomeScreen} />
+                  <Stack.Screen name="Entry" component={EntryScreen} />
+                  <Stack.Screen name="Statistics" component={StatisticsScreen} />
+                  <Stack.Screen name="Meditation" component={MeditationScreen} />
+                  <Stack.Screen name="Gallery" component={GalleryScreen} />
+                  <Stack.Screen name="Quote" component={QuoteScreen} />
+                  <Stack.Screen name="Options" component={OptionsScreen} />
+                  <Stack.Screen name="MeditationSession" component={MeditationSession} />
+              </Stack.Navigator>
+          </NavigationContainer>
+      </ThemeProvider>
   );
 }
